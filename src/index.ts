f93d065b12485c5bc2b7d5ccc034be997064cb42
HIeu <hieule1235@gmail.com>
2024-02-24 11:16:42 +0700

    test git rebase

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import cors from 'cors'
 import dotenv from 'dotenv'
 import express, { Express } from 'express'
-import userRoute from './routes/User.Routes'
 import novelRouter from './routes/Novels.Routes'
+import userRoute from './routes/User.Routes'
 import { databaseService } from './services/database.services'
 dotenv.config()
 
@@ -12,7 +12,7 @@ const port = process.env.PORT
 databaseService.run()
 app.use(express.json())
 app.use(cors())
-
+//test for rebase
 app.use('/users', userRoute)
 app.use('/novels', novelRouter)
 app.listen(port, () => {
