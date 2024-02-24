28144f8f7c0f5c9461c7c28ef980a88b26aa7019
HIeu <hieule1235@gmail.com>
2024-02-24 11:18:39 +0700

    test git rebase

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import express, { Express } from 'express'
 import novelRouter from './routes/Novels.Routes'
-import userRoute from './routes/User.Routes'
 import { databaseService } from './services/database.services'
 dotenv.config()
 
@@ -13,7 +12,7 @@ databaseService.run()
 app.use(express.json())
 app.use(cors())
 //test for rebase
-app.use('/users', userRoute)
+app.use('/users', novelRouter)
 app.use('/novels', novelRouter)
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`)
